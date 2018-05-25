68f0b57da933b9459399641d8ec54c42fb97ea67
SamanehPirveisi <samaneh_pirveisy@yahoo.com>
2018-05-25 16:19:20 +0200

    addin some example about date and time

--- src/dateExample/localDate.java
+++ src/dateExample/localDate.java
@@ -1,10 +1,13 @@
 package dateExample;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class localDate {
 
 	public static void main(String[] args) {
+		//
 
 		LocalDate date = LocalDate.now();
 	    LocalDate yesterday = date.minusDays(2);  
@@ -18,7 +21,27 @@ public class localDate {
 	    LocalDate date2 = LocalDate.of(2017,4, 15);  
 	    System.out.println(date2.isLeapYear());  
 	    
-	
+	    ////
+	    LocalDate date3 = LocalDate.of(2017, 1, 13);  
+	    LocalDateTime datetime = date3.atTime(1,50,9);      
+	    System.out.println(datetime);   
+	///
+	    
+	    LocalTime time = LocalTime.now();  
+	    System.out.println(date +"  "+time);
+	    
+	    ///
+	    LocalTime time2 = LocalTime.of(10,43,12);  
+	    System.out.println(date + " "+time2);  
+	    //
+	    LocalTime time3 = LocalTime.of(10,43,12);  
+	    System.out.println(time3);  
+	    LocalTime time4=time3.minusHours(2);  
+	    LocalTime time5=time4.minusMinutes(34);  
+	    System.out.println(time5);  
+	    
+	    
+	    
 	}
 
 }
